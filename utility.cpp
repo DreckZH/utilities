fc32f79b0c4d350d51d1f21d06d46d412076c2ed
DreckZH <zh-xh2006@163.com>
2016-11-10 14:45:42 +0800

    Add files via upload

---
+++ utility.cpp
@@ -0,0 +1,24 @@
+
+
+
+int calcCRC(const char * crcStartPtr, int  buffLen)
+{
+	int crc = 0xFFFF;
+	char * bufferTop = crcStartPtr + buffLen;
+	char * crcBuffPtr = crcStartPtr; 
+	int  i;
+
+	while (crcBuffPtr < bufferTop)
+	{
+		crc = (int)(crc & 0xff00) + (*(char *)crcBuffPtr ^ LOBYTE(crc));
+		for (i = 0; i<8; i++)
+		{
+			if ((crc & 0x0001) == TRUE)
+				crc = (crc >> 1) ^ 0xA001;
+			else
+				crc = (crc >> 1);
+		}
+		crcBuffPtr++;
+	}
+	return (crc);
+} 
